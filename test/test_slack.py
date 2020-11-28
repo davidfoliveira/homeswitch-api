dcf88cdf834ea0c346d925967f26d22838533088
David Oliveira <david@Davids-MacBook.lan>
2020-11-28 20:36:36 +0100

    Remove webhook url

--- test/test_slack.py
+++ test/test_slack.py
@@ -4,7 +4,7 @@ from homeswitch.device import Device
 def _on_get_response():
 	print("CALLBACK CALLED")
 
-slack = SlackHook({'url': 'https://hooks.slack.com/services/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'})
+slack = SlackHook({'url': 'https://hooks.slack.com/services/...'})
 slack.notify("status_update", data={
 	'device': Device('ID'),
 	'status': False,
